49301f168aa4fc422b7708035bf26b43d23b4cee
chad kidd <ckiddwebdev@gmail.com>
2019-03-07 15:49:15 -0800

    fixed some typos

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -10,7 +10,7 @@ class TabLink {
     // Using the custom data attribute get the associated Item element
     //referring to tab-items and data-tab pair //dont forget the dang dot
     //also be sure to use backticks not single quotes
-    this.itemElement = document.querySelector(`.tabs-items[data-tab="${this.data}"]`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
     //keyword new setting tabItem 'varaible' to this.itemElement which holds data-tab
@@ -26,7 +26,7 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-links.forEach(function(current) {
+  links.forEach(function(current) {
   current.classList.remove("tabs-link-selected");
 })
     // Add a class named "tabs-link-selected" to this link
@@ -66,7 +66,7 @@ class TabItem {
 
 
 
-links = document.querySelectorAll('.tabs-link');
+let links = document.querySelectorAll('.tabs-link');
 links.forEach(function(link) {
   return new TabLink(link);
 });
